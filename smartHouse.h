73f05984a876dc62c95de6d956d74cbfa7f6ff6a
Marek Husar <marek.husar@yahoo.com>
2017-04-30 01:17:07 +0200

    changes

--- smartHouse.h
+++ smartHouse.h
@@ -2,26 +2,17 @@
 #ifndef Morse_h
 #define Morse_h
 
-#include "Arduino.h"
-
-#ifndef INT32U
-#define INT32U unsigned long
-#endif
-
-#ifndef INT8U
-#define INT8U byte
-#endif
-
+#include <Arduino.h>
 
 struct MASK_FILTER {
-	INT32U maskRXM0;
-	INT32U filterRXF0;
-	INT32U filterFXR1;
-	INT32U maskRXM1;
-	INT32U filterRXF2;
-	INT32U filterRXF3;
-	INT32U filterRXF4;
-	INT32U filterRXF5;
+	uint32_t maskRXM0;
+	uint32_t filterRXF0;
+	uint32_t filterFXR1;
+	uint32_t maskRXM1;
+	uint32_t filterRXF2;
+	uint32_t filterRXF3;
+	uint32_t filterRXF4;
+	uint32_t filterRXF5;
 };
 
 #define MAC_ADDRESS_LENGTH 16	//* length of MAC address in bits
@@ -42,12 +33,12 @@ struct MASK_FILTER {
 #define CANID_END_TYPE_IN_ID 23
 #define CANID_LENGTH_OF_TYPE 8
 
-#define CANID_TYPE_NO_SPECIFICATION 0
-#define CANID_TYPE_GET_WHOLE_CONF 1
-#define CANID_TYPE_SEND_WHOLE_CONF 2
-#define CANID_TYPE_ADD_CONF 3
-#define CANID_TYPE_DEL_CONF 4
-#define CANID_TYPE_SWITCH_SEND 5
+#define CANID_MSGTYPE_NO_SPECIFICATION 0
+#define CANID_MSGTYPE_WHOLE_CONF 1
+#define CANID_MSGTYPE_SEND_WHOLE_CONF 2
+#define CANID_MSGTYPE_ADD_CONF 3
+#define CANID_MSGTYPE_DEL_CONF 4
+#define CANID_MSGTYPE_SWITCH_SEND 5
 
 enum DEVICE_TYPE { switchButton, pushButton, stairCaseSwitch, light, lightWithDimmer, socket };
 enum MESSAGE_TYPE { configRequest, configResponse, eventFromSwitch, eventFromPushButton };
@@ -55,33 +46,34 @@ enum MESSAGE_TYPE { configRequest, configResponse, eventFromSwitch, eventFromPus
 enum ROUTABLE_MESSAGES { routable, noRoutable };
 
 struct CONF_DEVICE {
-	INT32U macID;		//* Identifikator z CanBus zariadenia (z EEPROM)
+	uint32_t macID;		//* Identifikator z CanBus zariadenia (z EEPROM)
 	//MASK_FILTER masksAndFilters;
 	byte canSpeed;
-	INT32U segment;
+	uint32_t segment;
 	DEVICE_TYPE deviceType;
 };
 
 struct CONF_MESSAGE {
-	CONF_MESSAGE(INT32U macID, unsigned char length, byte confData[]) {
-		_macID = macID;
-		_length = length;
-		for (int i = 0; i < length; i++) {
-			_confData[i] = confData[i];
-		}
-	};
-	CONF_MESSAGE() {
-		_macID = 0;
-		_length = 0; 
-	};
-
-	INT32U _macID;	//* Identifikator z CanBus zariadenia (z EEPROM)
+	uint16_t _macID;	//* Identifikator z CanBus zariadenia (z EEPROM)
 	byte _confData[9];
 	unsigned char _length;
 	//DEVICE_TYPE deviceType;	//* urcenie zariadenia vzhladom na GPIO pin
 	//INT8U gpio;		//* pin, ktory je pouzity (pri ziarovke/zasuvke ako vystupny, pri vypinaci ako vstupny, podla deviceType)
 	//INT32U canID;			//* ID spravy, ktore bude poslane pri udalosti. ked to bude vypinac, tak bude poslana sprava s tymto ID a ziarovky/zasuvky to budu odchytavat
 	//ROUTABLE_MESSAGES routable;	//* urci, ci sprava bude moct byt presmerovana do inych segmentov siete
+	
+	CONF_MESSAGE(uint16_t & macID, unsigned char length, byte confData[]) {
+		_macID = macID;
+		_length = length;
+		for (int i = 0; i < length; i++) {
+			_confData[i] = confData[i];
+		}
+	};
+
+	CONF_MESSAGE() {
+		_macID = 0;
+		_length = 0;
+	};
 };
 
 
@@ -93,15 +85,19 @@ public:
 
 class CanExt {
 public:
-	static bool isMessageFromCanConf(INT32U & id) {
+	static bool isConfigurationMsg(uint32_t & id) {
 		//* nastavime masku 16711680 = 0000 0000 ‭1111 1111 0000 0000 0000 0000‬
 		int mask = 16711680;
 		//* posunieme o 16 miest do prava, cize posunieme bity do prveho byte 
 		uint8_t res = (id & mask) >> 16;
-		return (res == CANID_TYPE_GET_WHOLE_CONF);
+		if (res == CANID_MSGTYPE_WHOLE_CONF || res == CANID_MSGTYPE_ADD_CONF || res == CANID_MSGTYPE_DEL_CONF) {
+			return true;
+		} else {
+			return false;
+		}
 	}
 
-	static INT32U getNormalizedID(INT32U id) {
+	static uint16_t getDeviceID(uint32_t id) {
 		//* vycisti IDcko od bitov, ktore su konfiguracne, ktore nie su pre identifikator
 		//* 65535 = 0000 0000 0000 0000 1111 1111 1111 1111
 		int mask = 65535;
